01ab9d837247fa5c26a9d1b892d4ee7ece943c86
Flabby <flabby@orionlife.net>
2018-07-03 17:39:40 -0400

    Rcon Testing

--- routes/Backend/servers.js
+++ routes/Backend/servers.js
@@ -1,5 +1,12 @@
 module.exports = [
     {
+        "Name": "MaldenLife",
+        "IP": "66.70.180.170",
+        "Port": "2302",
+        "RCONPort": "2310",
+        "RCONPassword": "49e0fe27dc8ec85b8898519810a78b5f"
+    },
+    {
         "Name": "MaldenLife-Test",
         "IP": "66.70.180.170",
         "Port": "2322",
