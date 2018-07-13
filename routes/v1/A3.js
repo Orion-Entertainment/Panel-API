c918c2b21497290bd6cac1a97baa2a32e9895b60
Flabby <flabby@orionlife.net>
2018-07-13 19:48:53 -0400

    Update

--- routes/v1/A3.js
+++ routes/v1/A3.js
@@ -14,11 +14,39 @@ router.post('/Check', async(req, res, next) => {
         const CheckLogin = await req.Check(req.body["client_id"], req.body["token"]);
         if (CheckLogin == false) {
             return res.send("Invalid Login");
+        } else if (req.body["option"] == undefined | req.body["data"] == undefined) {
+            return res.send("Invalid Login");
         }
 
-        console.log(req.headers['cf-connecting-ip']);
-        console.log(req.body);
         return res.send("Success");
+    } catch (error) {
+        console.log(error)
+        return res.send("API Error");
+    }
+});
+
+//For Arma 3 Server Extension
+router.post('/Addon', async(req, res, next) => {
+    try {
+        /* Check Login */
+        const CheckLogin = await req.Check(req.body["client_id"], req.body["token"]);
+        if (CheckLogin == false) {
+            return res.send("Invalid Login");
+        } else if (req.body["option"] == undefined | req.body["data"] == undefined) {
+            return res.send("Invalid Login");
+        }
+        const Option = req.body["option"].replace(/["]/g, "");
+        console.log(Option)
+
+        switch (Option) {
+            case "Log":
+                console.log('log 1')
+                return res.send("Success");
+            
+            default:
+                return res.send("Invalid Option");
+        }
+        
     } catch (error) {
         console.log(error)
         return res.send("API Error");
