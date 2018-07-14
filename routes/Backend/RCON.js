93fd833638ccec1f9389ceeae2006cd28db3c3c6
Flabby <flabby@orionlife.net>
2018-07-14 15:11:04 -0400

    Add return

--- routes/Backend/RCON.js
+++ routes/Backend/RCON.js
@@ -196,24 +196,28 @@ async function checkPlayers(time) {
                                             else if (results[0] == undefined) {
                                                 API.query("INSERT INTO `rcon_players` (`Server`,`Name`,`IP`,`GUID`,`Ping`) VALUES(?,?,?,?,?);", [ServerName,Name,IP,GUID,Ping], function (error, results, fields) {
                                                     if (error) throw error;
+                                                    return;
                                                 });
                                             } else {
                                                 if (results[0].IP == null | results[0].IP == "") {
                                                     API.query("UPDATE `rcon_players` set `IP`=?,`Ping`=? WHERE `Server`=? AND `Name`=? AND `GUID`=?;", [IP,Ping,ServerName,Name,GUID], function (error, results, fields) {
                                                         if (error) throw error;
+                                                        return;
                                                     });
                                                 } else if (results[0].GUID == null | results[0].GUID == "") {
                                                     API.query("UPDATE `rcon_players` set `GUID`=?,`Ping`=? WHERE `Server`=? AND `Name`=? AND `IP`=?;", [GUID,Ping,ServerName,Name,IP], function (error, results, fields) {
                                                         if (error) throw error;
+                                                        return;
                                                     });
                                                 } else {
                                                     API.query("UPDATE `rcon_players` set `Ping`=? WHERE `Server`=? AND `Name`=?;", [Ping,ServerName,Name], function (error, results, fields) {
                                                         if (error) throw error;
+                                                        return;
                                                     });
                                                 }
                                             }
                                         });
-                                    }
+                                    } else {return}
                                 }
                             }
                         });
