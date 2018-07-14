c2f2799a1fbc47226e40007cce8a36c28e09c8b8
Flabby <flabby@orionlife.net>
2018-07-14 18:16:33 -0400

    Fix

--- routes/Backend/Database.js
+++ routes/Backend/Database.js
@@ -22,23 +22,23 @@ async function checkNewPlayers(time) {
     try {
         setTimeout(async function() {
             for (var i in ServerDBs) {
-                const DB = ServerDBs[Object.keys(ServerDBs)[i]];
+                const DB = i;
                 const Query = await DB.query("SELECT `name`,`pid` FROM `players` WHERE `Tracked`='0' LIMIT 250;");
-                
+
                 if (Query[0] !== undefined) {
                     for (let p = 0; p < Query.length; p++) {
                         const Now = await moment(new Date()).format('YYYY/MM/DD HH:mm:ss');
-                        const GUID = await pid2guid(Query[i].pid);
+                        const GUID = await pid2guid(Query[p].pid);
                         const CheckPlayer = await API.query("SELECT `Last Name`,`Names`,`Last IP`,`IPs` FROM `servers_players` WHERE BINARY `GUID`=?;", [GUID]);
                         if (CheckPlayer[0] == undefined) {
                             const Names = JSON.stringify([{
-                                [Query[i].name]: Now
+                                [Query[p].name]: Now
                             }])
-                            await API.query("INSERT INTO `servers_players` (`Last Name`,`Names`,`GUID`,`Steam64ID`,`First Seen`) VALUES(?,?,?,?,?);", [Query[i].name,Names,GUID,Query[i].pid,Now]);
+                            await API.query("INSERT INTO `servers_players` (`Last Name`,`Names`,`GUID`,`Steam64ID`,`First Seen`) VALUES(?,?,?,?,?);", [Query[p].name,Names,GUID,Query[p].pid,Now]);
                         } else if (CheckPlayer[0].Steam64ID == undefined) {
-                            await DB.query("UPDATE `servers_players` set `Steam64ID`=? WHERE BINARY `GUID`=?;", [Query[i].pid,GUID]);
+                            await DB.query("UPDATE `servers_players` set `Steam64ID`=? WHERE BINARY `GUID`=?;", [Query[p].pid,GUID]);
                         }
-                        await DB.query("UPDATE `players` set `Tracked`='1' WHERE BINARY `pid`=?;", [Query[i].pid]);
+                        await DB.query("UPDATE `players` set `Tracked`='1' WHERE BINARY `pid`=?;", [Query[p].pid]);
                         return;
                     }
                 } else {return}
