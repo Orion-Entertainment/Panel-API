56494636daa580bfb7918e6621def5cd2cf8ae5b
Flabby <flabby@orionlife.net>
2018-07-14 18:07:38 -0400

    Test

--- routes/Backend/Database.js
+++ routes/Backend/Database.js
@@ -18,29 +18,37 @@ const pid2guid = function(pid) {
 	return hash.toString();
 };
 
+let checkingPlayers = false;
 async function checkNewPlayers(time) {
     try {
         setTimeout(async function() {
-            for (let i = 0; i < ServerDBs.length; i++) {
-                const DB = ServerDBs[Object.keys(ServerDBs)[i]];
-                const Query = await DB.query("SELECT `name`,`pid` FROM `players` WHERE `Tracked`='0' LIMIT 250;");
-                if (Query[0] !== undefined) {
-                    for (let p = 0; p < Query.length; p++) {
-                        const Now = await moment(new Date()).format('YYYY/MM/DD HH:mm:ss');
-                        const GUID = await pid2guid(Query[i].pid);
-                        const CheckPlayer = await API.query("SELECT `Last Name`,`Names`,`Last IP`,`IPs` FROM `servers_players` WHERE BINARY `GUID`=?;", [GUID]);
-                        if (CheckPlayer[0] == undefined) {
-                            const Names = JSON.stringify([{
-                                [Query[i].name]: Now
-                            }])
-                            await API.query("INSERT INTO `servers_players` (`Last Name`,`Names`,`GUID`,`Steam64ID`,`First Seen`) VALUES(?,?,?,?,?);", [Query[i].name,Names,GUID,Query[i].pid,Now]);
-                        } else if (CheckPlayer[0].Steam64ID == undefined) {
-                            await DB.query("UPDATE `servers_players` set `Steam64ID`=? WHERE BINARY `GUID`=?;", [Query[i].pid,GUID]);
+            console.log(`check`)
+            if (checkingPlayers == false) {
+                checkingPlayers = true;
+                for (let i = 0; i < ServerDBs.length; i++) {
+                    const DB = ServerDBs[Object.keys(ServerDBs)[i]];
+                    const Query = await DB.query("SELECT `name`,`pid` FROM `players` WHERE `Tracked`='0' LIMIT 250;");
+                    if (Query[0] !== undefined) {
+                        for (let p = 0; p < Query.length; p++) {
+                            const Now = await moment(new Date()).format('YYYY/MM/DD HH:mm:ss');
+                            const GUID = await pid2guid(Query[i].pid);
+                            const CheckPlayer = await API.query("SELECT `Last Name`,`Names`,`Last IP`,`IPs` FROM `servers_players` WHERE BINARY `GUID`=?;", [GUID]);
+                            if (CheckPlayer[0] == undefined) {
+                                const Names = JSON.stringify([{
+                                    [Query[i].name]: Now
+                                }])
+                                await API.query("INSERT INTO `servers_players` (`Last Name`,`Names`,`GUID`,`Steam64ID`,`First Seen`) VALUES(?,?,?,?,?);", [Query[i].name,Names,GUID,Query[i].pid,Now]);
+                            } else if (CheckPlayer[0].Steam64ID == undefined) {
+                                await DB.query("UPDATE `servers_players` set `Steam64ID`=? WHERE BINARY `GUID`=?;", [Query[i].pid,GUID]);
+                            }
+                            await DB.query("UPDATE `players` set `Tracked`='1' WHERE BINARY `pid`=?;", [Query[i].pid]);
+                            return;
                         }
-                        await DB.query("UPDATE `players` set `Tracked`='1' WHERE BINARY `pid`=?;", [Query[i].pid]);
-                        return;
+                    } else {return}
+                    if (i + 1 == ServerDBs.length) {
+                        checkingPlayers = false;
                     }
-                } else {return}
+                }
             }
             checkNewPlayers(time);
         }, time * 1000);
