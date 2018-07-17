895244c9af5a10cabf9d882b39f2b79133a4d0c2
Flabby <flabby@orionlife.net>
2018-07-17 17:47:33 -0400

    Test

--- routes/Backend/RCON.js
+++ routes/Backend/RCON.js
@@ -191,7 +191,7 @@ async function updatePlayer(Name, IP, GUID) {
         if (Player["Last Name"] !== Name) {
             let Names = [];
             if (Player["Names"] !== null) {
-                Names = JSON.parse(Player["Names"]);
+                Names = Player["Names"];
             } else {
                 Names = [];
             }
@@ -228,7 +228,7 @@ async function updatePlayer(Name, IP, GUID) {
         if (Player["Last IP"] !== IP) {
             let IPs = [];
             if (Player["IPs"] !== null) {
-                IPs = JSON.parse(Player["IPs"]);
+                IPs = Player["IPs"];
             } else {
                 IPs = [];
             }
