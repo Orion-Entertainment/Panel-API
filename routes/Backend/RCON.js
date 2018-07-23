452ac2df21c86fee9e2dd2ae72922f51c5c5cb11
Flabby <flabby@orionlife.net>
2018-07-22 21:21:22 -0400

    Fix auto ban

--- routes/Backend/RCON.js
+++ routes/Backend/RCON.js
@@ -144,7 +144,7 @@ async function connectRCon (BEConfig, ServerName) {
 
                 if (/.+ Restriction #\d+/.test(getData[3])) {
                     //Add ban for hacking
-                    API.query("INSERT INTO `arma_bans` (`GUID`,`Notes`,`Reason`) VALUES(?,?);", [getData[2],getData[3],"+Flabby - Perm - Hacking"], function (error, results, fields) {
+                    API.query("INSERT INTO `arma_bans` (`GUID`,`Notes`,`Reason`) VALUES(?,?,?);", [getData[2],getData[3],"+Flabby - Perm - Hacking"], function (error, results, fields) {
                         if (error) throw error;
                     });
                 }
