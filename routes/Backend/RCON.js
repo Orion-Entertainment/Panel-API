8a68a3c707852e121c563b5535129e4f7c8c6bcc
Flabby <flabby@orionlife.net>
2018-07-22 20:44:04 -0400

    w

--- routes/Backend/RCON.js
+++ routes/Backend/RCON.js
@@ -189,11 +189,12 @@ async function getPlayerGUID(ServerName, Name) {
 
 async function getPlayerID(ServerName, GUID, checkID) {
     if (checkID >= 5) return false;
+    checkID++
     const query = await API.query("SELECT `ID` FROM `arma_liveplayers` WHERE BINARY `Server`=? AND BINARY `GUID`=?;", [ServerName,GUID]);
     if (query[0] == undefined) {
-        setTimeout(getPlayerID(ServerName, GUID, checkID+1), 1000);
+        setTimeout(getPlayerID(ServerName, GUID, checkID), 1000);
     } else if (query[0].ID == null) {
-        setTimeout(getPlayerID(ServerName, GUID, checkID+1), 1000);
+        setTimeout(getPlayerID(ServerName, GUID, checkID), 1000);
     } else {
         console.log(query[0])
         return query[0].ID
