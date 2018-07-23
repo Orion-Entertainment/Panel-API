f6585a8af894244b9a27775fc607810a151055e1
Flabby <flabby@orionlife.net>
2018-07-22 23:24:45 -0400

    Fixes

--- routes/v1/A3.js
+++ routes/v1/A3.js
@@ -37,68 +37,67 @@ router.post('/Addon', async(req, res, next) => {
         }
 
         const TokenData = await req.GetData(req.body["client_id"], req.body["token"]);
-        if (TokenData == false) return res.send("Invalid Login")
-        else {
-            if (TokenData.Server == undefined) return res.send("Invalid Login")
-            else {
-                server = TokenData.Server
-                const Option = JSON.parse(req.body["option"]);
-                if (/<NULL-object>|B Alpha 1-\d:\d+/g.test(req.body["data"])) {
-                    const fix = await req.body["data"].match(/(.+)(<NULL-object>|B Alpha 1-\d:\d+)(.+)/);
-                    Data = fix[1]+'""'+fix[3];
-                } else {
-                    Data = JSON.parse(req.body["data"]);
-                }
+        if (TokenData == undefined) {
+            return res.send("Invalid Login")
+        }else if (TokenData.Server == false) {
+            return res.send("Invalid Login")
+        } else {
+            server = TokenData.Server
+            const Option = JSON.parse(req.body["option"]);
+            if (/<NULL-object>|B Alpha 1-\d:\d+/g.test(req.body["data"])) {
+                const fix = await req.body["data"].match(/(.+)(<NULL-object>|B Alpha 1-\d:\d+)(.+)/);
+                Data = fix[1]+'""'+fix[3];
+            } else {
+                Data = JSON.parse(req.body["data"]);
+            }
 
-                switch (Option) {
-                    case "Log":
-                        const Action = Data[0];
-                        switch (Action) {
-                            case "Killed":
-                                //const KilledName = Data[1];
-                                const KilledPID = Data[2];
-                                let KilledGroup = Data[3];
-                                // const KillerName = Data[4];
-                                let KillerPID = Data[5];
-                                let Weapon = Data[6];
-                                let KillerGroup = Data[7];
-                                let Distance = Data[8];
+            switch (Option) {
+                case "Log":
+                    const Action = Data[0];
+                    switch (Action) {
+                        case "Killed":
+                            //const KilledName = Data[1];
+                            const KilledPID = Data[2];
+                            let KilledGroup = Data[3];
+                            // const KillerName = Data[4];
+                            let KillerPID = Data[5];
+                            let Weapon = Data[6];
+                            let KillerGroup = Data[7];
+                            let Distance = Data[8];
 
-                                if (KilledGroup == "" | KilledGroup == "No Group" | KilledGroup == "No Gang") {
-                                    KilledGroup = null;
-                                };
-                                if (KillerPID === "") {
-                                    KillerPID = null;
-                                };
-                                if (KillerGroup == "" | KilledGroup == "No Group" | KilledGroup == "No Gang") {
-                                    KillerGroup = null;
-                                };
-                                if (Weapon == "") {
-                                    Weapon = null;
-                                };
-                                if (Distance < 0) {
-                                    Distance = 0;
-                                };
+                            if (KilledGroup == "" | KilledGroup == "No Group" | KilledGroup == "No Gang") {
+                                KilledGroup = null;
+                            };
+                            if (KillerPID === "") {
+                                KillerPID = null;
+                            };
+                            if (KillerGroup == "" | KilledGroup == "No Group" | KilledGroup == "No Gang") {
+                                KillerGroup = null;
+                            };
+                            if (Weapon == "") {
+                                Weapon = null;
+                            };
+                            if (Distance < 0) {
+                                Distance = 0;
+                            };
 
-                                req.API.query("INSERT INTO `arma_kills` (`Server`,`Killer`,`KillerGroup`,`Killed`,`KilledGroup`,`Weapon`,`Distance`) VALUES(?,?,?,?,?,?,?);", [server,KillerPID,KillerGroup,KilledPID,KilledGroup,Weapon,Distance]);
-                                return res.send("Success");
+                            req.API.query("INSERT INTO `arma_kills` (`Server`,`Killer`,`KillerGroup`,`Killed`,`KilledGroup`,`Weapon`,`Distance`) VALUES(?,?,?,?,?,?,?);", [server,KillerPID,KillerGroup,KilledPID,KilledGroup,Weapon,Distance]);
+                            return res.send("Success");
 
-                            default:
-                                console.log("Invalid Log Action: "+Action)
-                                console.log(req.body["data"])
-                                return res.send("Invalid Log Action: "+Action);
-                        }
-                    
-                    default:
-                        return res.send("Invalid Option: "+Option);
-                }
-            };
-        }
+                        default:
+                            console.log("Invalid Log Action: "+Action)
+                            console.log(req.body["data"])
+                            return res.send("Invalid Log Action: "+Action);
+                    }
+                
+                default:
+                    return res.send("Invalid Option: "+Option);
+            }
+        };
     } catch (error) {
         return res.send("API Error");
     }
 });
-
 
 router.post('/Killfeed', async(req, res, next) => {
     try {
@@ -110,13 +109,14 @@ router.post('/Killfeed', async(req, res, next) => {
             return res.send("Invalid Login");
         }
 
-        const getRecentKills = req.API.query("SELECT * FROM `arma_kills` WHERE", []);
-        if (getRecentKills[0] == undefined) {
-            return [];
-        } else {
-
+        if (req.body.server == undefined) {
+            const getRecentKills = req.API.query("SELECT * FROM `arma_kills` ORDER BY `Time` DESC LIMIT 20");
+            if (getRecentKills[0] == undefined) {
+                return false;
+            } else {
+                /* Will comeback to this tomorrow once there is more kills logged */
+            }
         }
-
     } catch (error) {
         console.log(error)
         return res.send("API Error");
