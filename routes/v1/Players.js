b13c788e666337c4063db03661123e889448cdbb
Flabby <flabby@orionlife.net>
2018-08-06 00:05:33 -0400

    Get Bans

--- routes/v1/Players.js
+++ routes/v1/Players.js
@@ -89,34 +89,68 @@ router.post('/Info', async(req, res, next) => {
         else if (JSON.parse(TokenData).Panel == undefined) return res.json({Error: "Access Denied"})
         else if (JSON.parse(TokenData).Panel !== true) return res.json({Error: "Access Denied"})
 
-        if (req.body.PlayerID == undefined) return res.json({Error: "PlayerID Undefined"})
+        else if (req.body.PlayerID == undefined) return res.json({Error: "PlayerID Undefined"})
         const PlayerID = req.body.PlayerID;
-        if (PlayerID == "" | isNaN(PlayerID)) return res.json({Error: "PlayerID Invalid"})
-        req.API.query("SELECT `id`,`Last Name`,`Names`,`Steam64ID`,`GUID`,`First Seen`,`Last Seen` FROM `arma_players` WHERE `id`=?;", [PlayerID], async function (error, results, fields) {
-            if (error) {
-                console.error(error)
-                return res.json({Error: error})
-            }
-            
-            if (results[0] == undefined) {
-                return res.json({
-                    "Info": false
-                }).end();
-            } else {
-                const Result = results[0];
-                return res.json({
-                    "Info": {
-                        "id": Result["id"],
-                        "LastName": Result["Last Name"],
-                        "Names": JSON.parse(Result["Names"]),
-                        "Steam64ID": Result["Steam64ID"],
-                        "GUID": Result["GUID"],
-                        "FirstSeen": await moment(Result["First Seen"]).format('YYYY/MM/DD HH:mm:ss'),
-                        "LastSeen": await moment(Result["Last Seen"]).format('YYYY/MM/DD HH:mm:ss')
+
+        if (req.body.Option == undefined) {
+            if (PlayerID == "" | isNaN(PlayerID)) return res.json({Error: "PlayerID Invalid"})
+            req.API.query("SELECT `id`,`Last Name`,`Names`,`Steam64ID`,`GUID`,`First Seen`,`Last Seen` FROM `arma_players` WHERE `id`=?;", [PlayerID], async function (error, results, fields) {
+                if (error) {
+                    console.error(error)
+                    return res.json({Error: error})
+                } else if (results[0] == undefined) {
+                    return res.json({
+                        "Info": false
+                    }).end();
+                } else {
+                    const Result = results[0];
+                    return res.json({
+                        "Info": {
+                            "id": Result["id"],
+                            "LastName": Result["Last Name"],
+                            "Names": JSON.parse(Result["Names"]),
+                            "Steam64ID": Result["Steam64ID"],
+                            "GUID": Result["GUID"],
+                            "FirstSeen": await moment(Result["First Seen"]).format('YYYY/MM/DD HH:mm:ss'),
+                            "LastSeen": await moment(Result["Last Seen"]).format('YYYY/MM/DD HH:mm:ss')
+                        }
+                    }).end();
+                }
+            });
+        } else {
+            if (req.body.Option == "") return res.json({Error: "Option Invalid"})
+
+            switch (req.body.Option) {
+                case "Get":
+                    if (req.body.Option2 == undefined) return res.json({Error: "Option2 Undefined"})
+                    else if (req.body.Option2 == "") return res.json({Error: "Option2 Invalid"})
+
+                    switch (req.body.Option2) {
+                        case "Bans":
+                            const getGUID = await API.query("SELECT `GUID` FROM `arma_players` WHERE BINARY `id`=?", [PlayerID]);
+                            if (getGUID[0] == undefined) return res.json({Error: "Failed Getting GUID"})
+                            req.API.query("SELECT `id`,`Server`,`Reason`,`Created`,`Expires` FROM `arma_bans` WHERE BINARY `GUID`=?;", [getGUID[0].GUID], async function (error, results, fields) {
+                                if (error) {
+                                    console.error(error)
+                                    return res.json({Error: error})
+                                } else if (results[0] == undefined) {
+                                    return res.json({
+                                        "Bans": false
+                                    }).end();
+                                } else {
+                                    return res.send(results).end();
+                                }
+                            });
+
+                        default: 
+                            return res.json({Error: "Invalid Option2"})
                     }
-                }).end();
+
+                default: 
+                    return res.json({Error: "Invalid Option"})
             }
-        });
+        }
+        
     } catch (error) {
         console.log(error)
         return res.json({Error: "Error"})
