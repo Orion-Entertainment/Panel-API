f23710ce3f581f04cd6b25c93b1e1471d87c95c3
Flabby <flabby@orionlife.net>
2018-08-06 12:26:08 -0400

    fix

--- routes/v1/Players.js
+++ routes/v1/Players.js
@@ -4,6 +4,7 @@ const moment = require('moment');
 
 /* Set Variables */
 const ServerDBs = require('../../core/app').ServerDBs;
+const MaldenLife = ServerDBs.maldenlife;
 
 /* Added NPM Packages */
 
@@ -262,7 +263,7 @@ router.post('/Info', async(req, res, next) => {
                                     "MaldenLife": false
                                 }).end();
                             }
-                            ServerDBs.maldenlife2.query("SELECT SUM(`bankacc`,`cash`) AS 'Money',`coplevel`,`mediclevel`,`donorlevel`,`exp_level`,`exp_total`,`exp_perkPoints` FROM `players` WHERE BINARY `pid`=? LIMIT 1;", [Steam64ID], async function (error, results, fields) {
+                            MaldenLife.query("SELECT SUM(`bankacc`,`cash`) AS 'Money',`coplevel`,`mediclevel`,`donorlevel`,`exp_level`,`exp_total`,`exp_perkPoints` FROM `players` WHERE BINARY `pid`=? LIMIT 1;", [Steam64ID], async function (error, results, fields) {
                                 if (error) {
                                     console.error(error)
                                     return res.json({Error: error})
