91912c59c8846b24074edbdb4505e31ac5c4afbf
Flabby <flabby@orionlife.net>
2018-08-08 00:58:48 -0400

    log

--- routes/Backend/RCON.js
+++ routes/Backend/RCON.js
@@ -208,6 +208,7 @@ async function checkForBan(ServerName, GUID) {
         const Created = await moment(query[0].Created);
         const Expires = await moment(query[0].Expires);
         const diff = await Created.diff(Expires);
+        console.log(diff)
         if (diff < 0) {
             await API.query("UPDATE `arma_bans` set `Expired`='true' WHERE `id`=?;", [query[0].id]);
             return false;
