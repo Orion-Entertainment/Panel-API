9ee9efc612e618785dadcb51c813dfc28119f215
Flabby <flabby@orionlife.net>
2018-08-09 14:07:28 -0400

    Add option

--- routes/v1/Shop.js
+++ routes/v1/Shop.js
@@ -90,7 +90,7 @@ router.post('/Category', async(req, res, next) => {
         const CheckCategory = await req.API.query("SELECT `id` FROM `shop_categories` WHERE BINARY `Name`=? AND `Active`='True';", [req.body.Category]);
         if (CheckCategory[0] == undefined) return res.json({Error: "Category Not Found"})
 
-        req.API.query("SELECT `id`,`Name`,`IMG`,`Price`,`ShortDescription` FROM `shop_items` WHERE `Active`='True' AND BINARY `Category`=? ORDER BY `id` ASC;", [req.body.Category], async function (error, results, fields) {
+        req.API.query("SELECT `id`,`Name`,`IMG`,`Price`,`Option`,`ShortDescription` FROM `shop_items` WHERE `Active`='True' AND BINARY `Category`=? ORDER BY `id` ASC;", [req.body.Category], async function (error, results, fields) {
             if (error) {
                 console.error(error)
                 return res.json({Error: error})
