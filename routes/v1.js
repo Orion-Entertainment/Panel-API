b67744c4c99810aa268f01054ddc9fd99cd80adf
Flabby <flabby@orionlife.net>
2018-08-09 12:43:52 -0400

    Shop Test

--- routes/v1.js
+++ routes/v1.js
@@ -5,5 +5,6 @@ const API = require('./v1/API'); app.use('/API', API);
 const Login = require('./v1/Login'); app.use('/Login', Login);
 const A3 = require('./v1/A3'); app.use('/A3', A3);
 const Players = require('./v1/Players'); app.use('/Players', Players);
+const Shop = require('./v1/Shop'); app.use('/Shop', Shop);
 
 module.exports = app;
