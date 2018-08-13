37a23ad2b79acdc0c5d2c637215f470465f66a2c
Flabby <flabby@orionlife.net>
2018-08-13 02:03:33 -0400

    f

--- routes/v1/Changelog.js
+++ routes/v1/Changelog.js
@@ -123,6 +123,35 @@ router.post('/View', async(req, res, next) => {
                 console.error(error)
                 return res.json({Error: error})
             } else if(results[0] == undefined) return res.json({Error: "Changelog not found"}); else {
+                return res.send(results);
+            }
+        });
+    } catch (error) {
+        console.log(error)
+        return res.json({Error: "Error"})
+    }
+});
+
+router.post('/Edit', async(req, res, next) => {
+    try {
+        /* Check Login */
+        const CheckLogin = await req.Check(req.body["client_id"], req.body["token"]);
+        if (CheckLogin == false) return res.send("Invalid Login"); 
+        const TokenData = await req.GetData(req.body["client_id"], req.body["token"]);
+
+        if (TokenData == undefined) return res.json({Error: "Access Denied"})
+        else if (JSON.parse(TokenData).Panel == undefined) return res.json({Error: "Access Denied"})
+        else if (JSON.parse(TokenData).Panel !== true) return res.json({Error: "Access Denied"})
+
+        if (req.body.ID == undefined) return res.json({Error: "ID Undefined"})
+        else if (req.body.ID == "") return res.json({Error: "ID Empty"})
+        else if (isNaN(req.body.ID)) return res.json({Error: "ID Invalid"})
+
+        req.API.query("SELECT `id`,`Category`,`Name`,`Data`,`Time` FROM `changelogs` WHERE `Time`<NOW() AND `id`=? LIMIT 1;", [req.body.ID], async function (error, results, fields) {
+            if (error) {
+                console.error(error)
+                return res.json({Error: error})
+            } else if(results[0] == undefined) return res.json({Error: "Changelog not found"}); else {
                 return res.json({
                     ID: results[0].id,
                     Category: results[0].Category,
