39d71dfa595ff53072ec5ea00412eef8a8be396b
Flabby <flabby@orionlife.net>
2018-08-14 00:15:38 -0400

    Kills

--- routes/v1/Home.js
+++ routes/v1/Home.js
@@ -20,10 +20,17 @@ router.post('/GetData', async(req, res, next) => {
         else if (JSON.parse(TokenData).Panel == undefined) return res.json({Error: "Access Denied"})
         else if (JSON.parse(TokenData).Panel !== true) return res.json({Error: "Access Denied"})
 
+        /* Players */
         const totalPlayers = await req.API.query("SELECT COUNT(`id`) AS 'Total' FROM `arma_players` LIMIT 1;");
         const totalPlayersNewMonth = await req.API.query("SELECT COUNT(`id`) AS 'Total' FROM `arma_players` WHERE (`First Seen` > NOW() - INTERVAL 1 MONTH) LIMIT 1;");
         const totalPlayersMonth = await req.API.query("SELECT COUNT(`id`) AS 'Total' FROM `arma_players` WHERE (`Last Seen` > NOW() - INTERVAL 1 MONTH) LIMIT 1;");
         const totalBans = await req.API.query("SELECT COUNT(`id`) AS 'Total' FROM `arma_bans` WHERE (`Created` > NOW() - INTERVAL 1 MONTH) LIMIT 1;");
+
+        /* Kills */
+        const totalKills = await req.API.query("SELECT COUNT(`id`) AS 'Total' FROM `arma_kills` WHERE `killer`!=`killed` LIMIT 1;");
+        const totalKillsCop = await req.API.query("SELECT COUNT(`id`) AS 'Total' FROM `arma_kills` WHERE `killer` IS NOT NULL AND `killer`!=`killed` AND `KillerGroup`='Police' LIMIT 1;");
+        const totalKillsCivilians = await req.API.query("SELECT COUNT(`id`) AS 'Total' FROM `arma_kills` WHERE `killer` IS NOT NULL AND `killer`!=`killed` AND `KillerGroup`!='Police' LIMIT 1;");
+        const totalKillsSuicide = await req.API.query("SELECT COUNT(`id`) AS 'Total' FROM `arma_kills` WHERE `killer`=`killed` LIMIT 1;");
 
         return res.json({
             TotalPlayers: {
@@ -32,6 +39,13 @@ router.post('/GetData', async(req, res, next) => {
                 TotalNewMonth: totalPlayersNewMonth[0].Total,
                 TotalMonth: totalPlayersMonth[0].Total,
                 TotalBans: totalBans[0].Total
+            },
+            Kills: {
+                Total: totalKills[0].Total.toLocaleString(),
+
+                Cops: totalKillsCop[0].Total,
+                Civilians: totalKillsCivilians[0].Total,
+                Suicide: totalKillsSuicide[0].Total
             }
         });
     } catch (error) {
