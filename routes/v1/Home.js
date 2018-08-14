bc435abed7b1a6914f44a7f38e9a2987dae9d80d
Flabby <flabby@orionlife.net>
2018-08-13 23:39:18 -0400

    fix

--- routes/v1/Home.js
+++ routes/v1/Home.js
@@ -20,7 +20,7 @@ router.post('/GetData', async(req, res, next) => {
         else if (JSON.parse(TokenData).Panel == undefined) return res.json({Error: "Access Denied"})
         else if (JSON.parse(TokenData).Panel !== true) return res.json({Error: "Access Denied"})
 
-        const totalPlayers = req.API.query("SELECT COUNT(`id`) AS 'Total' FROM `arma_players` LIMIT 1;");
+        const totalPlayers = await req.API.query("SELECT COUNT(`id`) AS 'Total' FROM `arma_players` LIMIT 1;");
 
         return res.json({
             TotalPlayers: {
